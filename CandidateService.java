2b40fac12eeaa0eeee39a2d4dfbd12e21e225e85
eirini-str <t8230145@aueb.gr>
2024-12-20 18:52:07 +0200

    Αλλαγές στο CandidateService.java

--- CandidateService.java
+++ CandidateService.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CandidateService {
@@ -8,30 +11,6 @@ public class CandidateService {
     Map<String, Map<String, Integer>> numbers = Database.loadTableData(allTablesData, Map<?>.class);
     double[] weight = Database.loadTableData(Weight);
     double[][] points = new double[candidate.size()][weight.length];
-
-    public record Candidate(
-    String fullName,
-    String undergraduateUniversity,
-    String undergraduateDepartment,
-    int undergraduateGrade,
-    String masterUniversity,
-    String masterDepartment,
-    int masterGrade,
-    String phdUniversity,
-    String phdDepartment,
-    int phdGrade,
-    String englishLevel,
-    String frenchLevel,
-    String germanLevel,
-    String spanishLevel,
-    String chineseLevel,
-    String otherLanguageLevel,
-    int workExperienceYears,
-    String officeSkills,
-    String programmingLanguage
-    ) {}
-
-
 
     public double[][] reviewCandidates() {
         double[][] finalCandidates = new double[points.length][2];
@@ -61,26 +40,66 @@ public class CandidateService {
         return points;
     } 
 
-    public static double[] compareCandidateWithNumbers(Map<String, String> cand, Map<String, Map<String, Integer>> numbers) {
-        
-        String[] fieldsToCompare = {
-        "undergraduateUniversity",
-        "undergraduateDepartment",
-        "masterUniversity",
-        "masterDepartment",
-        "phdUniversity",
-        "phdDepartment",
-        "englishLevel",
-        "frenchLevel",
-        "germanLevel",
-        "spanishLevel",
-        "chineseLevel",
-        "otherLanguageLevel",
-        "workExperienceYears", 
-        "officeSkills",
-        "programmingLanguage"};
-
+    public static double[] compareCandidateWithNumbers(Map<String, Object> cand, Map<String, Map<String, Integer>> numbers, double uniqueId) {
+        List<Double> matchedValuesList = new ArrayList<>();
+    
+        // Αποθήκευση του fullName ως μοναδικό ID στην πρώτη θέση του πίνακα
+        matchedValuesList.add(uniqueId); // Τοποθετούμε το uniqueId στην πρώτη θέση
+    
+        // Πεδίο -> Κατηγορία σύγκρισης
+        Map<String, String> fieldToCategory = Map.of(
+            "undergraduateUniversity", "universities",
+            "masterUniversity", "universities",
+            "phdUniversity", "universities",
+            "undergraduateDepartment", "bachelorDept",
+            "masterDepartment", "masterDept",
+            "phdDepartment", "phDDept",
+            "englishLevel", "levels",
+            "frenchLevel", "levels",
+            "germanLevel", "levels",
+            "spanishLevel", "levels",
+            "chineseLevel", "levels",
+            "otherLanguageLevel", "levels",
+            "officeSkills", "levels",
+            "workExperienceYears", "workExperience",
+            "programmingLanguage", "yesNo"
+        );
+    
+        // Διατρέχουμε όλα τα πεδία προς σύγκριση
+        for (Map.Entry<String, String> entry : fieldToCategory.entrySet()) {
+            String field = entry.getKey();
+            String category = entry.getValue();
+    
+            if (cand.containsKey(field)) {
+                Object candValue = cand.get(field);
+                if (candValue instanceof String) {
+                    String stringValue = (String) candValue;
+                    if (numbers.containsKey(category)) {
+                        Map<String, Integer> categoryData = numbers.get(category);
+                        if (categoryData.containsKey(stringValue)) {
+                            matchedValuesList.add((double) categoryData.get(stringValue));
+                        } else {
+                            matchedValuesList.add(0.0); // Αν δεν υπάρχει τιμή, προσθέτουμε 0.0
+                        }
+                    } else {
+                        matchedValuesList.add(0.0); // Αν δεν υπάρχει κατηγορία, προσθέτουμε 0.0
+                    }
+                } else if (candValue instanceof Integer) {
+                    // Εισάγουμε την τιμή όπως είναι (για τα int πεδία)
+                    matchedValuesList.add(((Integer) candValue).doubleValue());
+                } else {
+                    matchedValuesList.add(0.0); // Μηδενική τιμή για άγνωστους τύπους
+                }
+            } else {
+                // Αν το πεδίο δεν υπάρχει και είναι String, προσθέτουμε 0.0
+                matchedValuesList.add(0.0);
+            }
+        }
+    
+        // Μετατροπή λίστας σε πίνακα double[]
+        double[] matchedValues = matchedValuesList.stream().mapToDouble(Double::doubleValue).toArray();
+    
+        return matchedValues;
     }
-
-
+    
 }

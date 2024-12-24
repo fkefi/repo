82ed03db6b496fea93372cd9ab6f06bc4881aedb
Efthalia Fakorelli <t8230156@aueb.gr>
2024-12-24 19:25:01 +0200

    java package

--- myCVision/src/main/java/com/cybersolvers/mycvision/ResumeService.java
+++ myCVision/src/main/java/com/cybersolvers/mycvision/ResumeService.java
@@ -1,3 +1,11 @@
+package com.cybersolvers.mycvision;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.List;
+
 public class ResumeService {
 
         String[] levels = {"όχι", "μέτρια", "πολύ καλά", "τέλεια"};

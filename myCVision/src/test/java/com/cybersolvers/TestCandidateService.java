e218c7349095e1143226a44c2569663df0270a33
eirini-str <t8230145@aueb.gr>
2025-01-06 20:58:16 +0200

    created TestCandidateService

---
+++ myCVision/src/test/java/com/cybersolvers/TestCandidateService.java
@@ -0,0 +1,19 @@
+public package test.java.com.cybersolvers;
+
+class TestCandidateService {
+
+    public void setUp() {}
+
+    public void testContructor() {}
+
+    public void testReviewCandidates() {}
+
+    public void testCalculateScore() {}
+
+    public void testCreatePoints() {}
+
+    public void testCompareCandidateWithNumbers() {}
+
+    public void tearDown() {}
+
+}

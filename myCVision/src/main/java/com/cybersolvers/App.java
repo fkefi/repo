e5df4c038d8442c105069ad9924a6a2483270198
eirini-str <t8230145@aueb.gr>
2025-01-07 19:32:07 +0200

    added main

--- myCVision/src/main/java/com/cybersolvers/App.java
+++ myCVision/src/main/java/com/cybersolvers/App.java
@@ -1,13 +1,19 @@
 package com.cybersolvers;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Κλήση μεθοδού CandidateService για την δημηιουργία τελικού πίνακα και ταξινόμιση υποψηφίων
+        CandidateService service = new CandidateService();
+        double[][] results = service.reviewCandidates();
+
+        System.out.println("Reviewed Candidates:");
+        for (double[] candidate : results) {
+            System.out.println("Candidate ID: " + candidate[0] + ", Score: " + candidate[1]);
+        }
+
+        // Κλήση μεθόδου Mailer για αποστολή των αποτελεσμάτων στην εταιρία
+        
     }
 }

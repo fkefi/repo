9ef991e3e306097b983b0fc6ba8a12bc6aa0ceee
Irene Lianou <t8230077@aueb.gr>
2025-01-09 09:44:55 +0200

    prosethesa tin dimiourgia toy pinaka id

--- myCVision/src/main/java/com/cybersolvers/mycvision/CandidateService.java
+++ myCVision/src/main/java/com/cybersolvers/mycvision/CandidateService.java
@@ -1,7 +1,12 @@
-import com.cybersolvers.mycvision;
-import com.cybersolvers.mycvision.SQLiteHandler;
+package com.cybersolvers.mycvision;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
 import java.util.*;
+
+import com.cybersolvers.mycvision.JsonNode;
+import com.cybersolvers.mycvision.ObjectMapper;
 
 public class CandidateService {
     private String[][] id;
@@ -11,16 +16,25 @@ public class CandidateService {
     private int numberOfCandidates;
     private int numberOfCriteria;
     private double[][] points;
+
+    private String jsonFilePath = "E:\\myCVision\\mycv\\src\\resources\\cv\\output.json"; 
+    private String jdbcUrl = "C:\\cygwin64\\home\\irenelianou\\repo\\myCVision\\src\\main\\resources"; 
+
     SQLiteHandler handler = new SQLiteHandler("C:\\cygwin\\home\\estri\\myrepo\\repo\\myCVision\\src\\main\\resources");
 
     public CandidateService() {
-    this.id = handler.fetchTable(ID);
-    this.candidates = handler.fetchJsonAsMap(Candidates);
-    this.numbers = handler.fetchJsonAsMap(allTablesData);
-    this.weight = handler.fetchTable(Weight);
-    this.numberOfCandidates = candidates.size();
-    this.numberOfCriteria = weight.length;
-    this.points = createPoints();  // Κλήση της createPoints για να πάρεις τον πίνακα
+        this.id = handler.fetchTable("ID");
+        this.candidates = handler.fetchJsonAsMap("Candidates");
+        this.numbers = handler.fetchJsonAsMap("allTablesData");
+        this.weight = handler.fetchTable("Weight");
+        this.numberOfCandidates = candidates.size();
+        this.numberOfCriteria = weight.length;
+        this.points = createPoints();
+    }
+
+    public CandidateService(String jsonFilePath, String jdbcUrl) {
+        this.jsonFilePath = jsonFilePath;
+        this.jdbcUrl = jdbcUrl;
     }
 
     public double[][] reviewCandidates() {
@@ -35,7 +49,7 @@ public class CandidateService {
         Arrays.sort(finalCandidates, (a, b) -> Double.compare(b[1], a[1]));
         handler.insertArray("finalCandidates", finalCandidates, finalCandidates.length, 2);
         return finalCandidates;
-    } 
+    }
 
     private double calculateScore(int i) {
         double score = 0.0;
@@ -45,38 +59,28 @@ public class CandidateService {
         return score;
     }
 
-    private double[][] createPoints () {
+    private double[][] createPoints() {
         double[][] points = new double[numberOfCandidates][numberOfCriteria + 1];
         int index = 0;
 
-        // Επεξεργασία κάθε υποψηφίου
         for (Map.Entry<String, Map<String, Object>> entry : candidates.entrySet()) {
             Map<String, Object> cand = entry.getValue();
-
-            // Ανάκτηση του fullName από τον cand
             String fullName = (String) cand.get("fullName");
             double uniqueId = 0.0;
 
-            // Αναζήτηση στο id για το αντίστοιχο ID του fullName
             for (String[] idRow : id) {
                 if (idRow[0].equals(fullName)) {
-                    uniqueId = Double.parseDouble(idRow[1]); // Μετατροπή ID σε double
+                    uniqueId = Double.parseDouble(idRow[1]);
                     break;
                 }
             }
 
-            // Έλεγχος αν δεν βρέθηκε το ID
             if (uniqueId == 0.0) {
                 System.out.println("Warning: No matching ID found for fullName: " + fullName);
             }
 
-            // Κλήση της compareCandidateWithNumbers για τον συγκεκριμένο υποψήφιο
             double[] matchedValues = compareCandidateWithNumbers(cand, numbers, uniqueId);
-
-            // Αποθήκευση του αποτελέσματος στον πίνακα points
             points[index] = matchedValues;
-
-            // Ενημέρωση του δείκτη
             index++;
         }
 
@@ -85,30 +89,27 @@ public class CandidateService {
 
     private double[] compareCandidateWithNumbers(Map<String, Object> cand, Map<String, Map<String, Integer>> numbers, double uniqueId) {
         List<Double> matchedValuesList = new ArrayList<>();
+        matchedValuesList.add(uniqueId);
 
-
-        matchedValuesList.add(uniqueId); // Τοποθετούμε το uniqueId στην πρώτη θέση
-
-        // Πεδίο -> Κατηγορία σύγκρισης
         Map<String, String> fieldToCategory = new LinkedHashMap<>();
-            fieldToCategory.put("undergraduateUniversity", "universities");
-            fieldToCategory.put("undergraduateDepartment", "bachelorDept");
-            fieldToCategory.put("undergraduateGrade", null);     
-            fieldToCategory.put("masterUniversity", "universities");
-            fieldToCategory.put("masterDepartment", "masterDept");
-            fieldToCategory.put("masterGrade", null);
-            fieldToCategory.put("phdUniversity", "universities");
-            fieldToCategory.put("phdDepartment", "phDDept");
-            fieldToCategory.put("phdGrade", null);
-            fieldToCategory.put("englishLevel", "levels");
-            fieldToCategory.put("frenchLevel", "levels");
-            fieldToCategory.put("germanLevel", "levels");
-            fieldToCategory.put("spanishLevel", "levels");
-            fieldToCategory.put("chineseLevel", "levels");
-            fieldToCategory.put("otherLanguageLevel", "levels");
-            fieldToCategory.put("workExperienceYears", "workExperience");
-            fieldToCategory.put("officeSkills", "levels");
-            fieldToCategory.put("programmingLanguage", "yesNo");
+        fieldToCategory.put("undergraduateUniversity", "universities");
+        fieldToCategory.put("undergraduateDepartment", "bachelorDept");
+        fieldToCategory.put("undergraduateGrade", null);
+        fieldToCategory.put("masterUniversity", "universities");
+        fieldToCategory.put("masterDepartment", "masterDept");
+        fieldToCategory.put("masterGrade", null);
+        fieldToCategory.put("phdUniversity", "universities");
+        fieldToCategory.put("phdDepartment", "phDDept");
+        fieldToCategory.put("phdGrade", null);
+        fieldToCategory.put("englishLevel", "levels");
+        fieldToCategory.put("frenchLevel", "levels");
+        fieldToCategory.put("germanLevel", "levels");
+        fieldToCategory.put("spanishLevel", "levels");
+        fieldToCategory.put("chineseLevel", "levels");
+        fieldToCategory.put("otherLanguageLevel", "levels");
+        fieldToCategory.put("workExperienceYears", "workExperience");
+        fieldToCategory.put("officeSkills", "levels");
+        fieldToCategory.put("programmingLanguage", "yesNo");
 
         for (Map.Entry<String, String> entry : fieldToCategory.entrySet()) {
             String field = entry.getKey();
@@ -122,11 +123,11 @@ public class CandidateService {
                         Map<String, Integer> categoryData = numbers.get(category);
                         if (categoryData.containsKey(stringValue)) {
                             matchedValuesList.add((double) categoryData.get(stringValue));
-                        } else if ((field.equals("undergraduateUniversity") || field.equals("undergraduateDepartment") || 
-                        field.equals("masterUniversity") || field.equals("masterDepartment") || 
-                        field.equals("phdUniversity") || field.equals("phdDepartment")) && 
-                        !stringValue.equalsIgnoreCase("no")) {
-                       matchedValuesList.add(1.0);
+                        } else if ((field.equals("undergraduateUniversity") || field.equals("undergraduateDepartment") ||
+                                field.equals("masterUniversity") || field.equals("masterDepartment") ||
+                                field.equals("phdUniversity") || field.equals("phdDepartment")) &&
+                                !stringValue.equalsIgnoreCase("no")) {
+                            matchedValuesList.add(1.0);
                         } else {
                             System.out.println("Warning: No match found for value " + stringValue + " in category " + category);
                             matchedValuesList.add(0.0);
@@ -137,19 +138,71 @@ public class CandidateService {
                 } else if (candValue instanceof Integer) {
                     matchedValuesList.add(((Integer) candValue).doubleValue());
                 } else if (category == null && candValue instanceof Double) {
-                    // Εισαγωγή βαθμών (grades) όταν η κατηγορία είναι null και η τιμή είναι double
                     matchedValuesList.add((Double) candValue);
                 } else {
                     matchedValuesList.add(0.0);
                 }
             }
-            
         }
 
-        // Μετατροπή λίστας σε πίνακα double[]
-        double[] matchedValues = matchedValuesList.stream().mapToDouble(Double::doubleValue).toArray();
-
-        return matchedValues;
+        return matchedValuesList.stream().mapToDouble(Double::doubleValue).toArray();
     }
+    public void processCandidates() {
+        SQLiteHandler dbHandler = null;
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(new File(jsonFilePath));
+    
+            dbHandler = new SQLiteHandler(jdbcUrl);
+    
+            Map<String, String> candidateMap = new HashMap<>();
+            for (JsonNode candidateNode : rootNode) { 
 
+                JsonNode fullNameNode = candidateNode.path("fullName");
+    
+                if (!fullNameNode.isMissingNode()) { 
+                    String fullName = fullNameNode.asText();
+                    String code = generateRandomCode();
+                    candidateMap.put(fullName, code);
+                } else {
+                    System.err.println("Ο υποψήφιος δεν έχει πεδίο fullName.");
+                }
+            }
+    
+            dbHandler.insertJsonAsMap("candidates", candidateMap);
+            System.out.println("Τα δεδομένα εισήχθησαν επιτυχώς στη βάση δεδομένων.");
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (dbHandler != null) {
+                try {
+                    dbHandler.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+        public String generateRandomCode() {
+            String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            StringBuilder code = new StringBuilder();
+        
+            Random random = new Random();
+        
+        
+            for (int i = 0; i < 2; i++) {
+                int index = random.nextInt(letters.length());
+                code.append(letters.charAt(index));
+            }
+        
+        
+            for (int i = 0; i < 4; i++) {
+                int digit = random.nextInt(10);
+                code.append(digit);
+            }
+        
+            return code.toString();
+        
+    }
 }
+    

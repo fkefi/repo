b3b8955bd2b134f2ceb75cc96ceefe9022f058a9
Efthalia Fakorelli <t8230156@aueb.gr>
2025-01-13 15:41:27 +0200

    changes

--- myCVision/src/main/java/com/cybersolvers/mycvision/ResumeService.java
+++ myCVision/src/main/java/com/cybersolvers/mycvision/ResumeService.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class ResumeService {
     public final String[] levels = {"TELEIA", "POLY KALA", "KALA", "OXI"};
-    
+    public final String[] workExperience = {"0", "1", "2", "3", "4", "5", "6"};
 
     URL resource = ResumeService.class.getClassLoader().getResource("mydatabase.db");
     String dbPath = resource.getPath();
@@ -17,11 +17,10 @@ public class ResumeService {
 
     private final SQLiteHandler sqlitehandler;
     
-    public final String universities;
-    public final String workExperience;
-    public final String bachelorDept;
-    public final String masterDept;
-    public final String phDDept;
+    public final String[] universities;
+    public final String[] bachelorDept;
+    public final String[] masterDept;
+    public final String[] phDDept;
     
     public  final List<String[]> arrays;
     public final List<String> tableNames;
@@ -31,23 +30,22 @@ public class ResumeService {
         this.sqlitehandler = new SQLiteHandler(dbUrl);
         
         // Initialize database values
-        this.universities = (String) sqlitehandler.fetchTable("universities");
-        this.workExperience = (String) sqlitehandler.fetchTable("workExperience");
-        this.bachelorDept = (String) sqlitehandler.fetchTable("bachelorDept");
-        this.masterDept = (String) sqlitehandler.fetchTable("masterDept");
-        this.phDDept = (String) sqlitehandler.fetchTable("phDDept");
+        this.universities = (String[]) sqlitehandler.fetchTable("universities");
+        this.bachelorDept = (String[]) sqlitehandler.fetchTable("bachelorDept");
+        this.masterDept = (String[]) sqlitehandler.fetchTable("masterDept");
+        this.phDDept = (String[]) sqlitehandler.fetchTable("phDDept");
         
         // Initialize lists
         this.arrays = new ArrayList<>();
         this.tableNames = new ArrayList<>();
         
         // Add items to arrays
-        arrays.add(universities.split(","));  // Υποθέτω ότι το fetchTable επιστρέφει comma-separated string
+        arrays.add(universities);  // Υποθέτω ότι το fetchTable επιστρέφει comma-separated string
         arrays.add(levels);
-        arrays.add(workExperience.split(","));
-        arrays.add(bachelorDept.split(","));
-        arrays.add(masterDept.split(","));
-        arrays.add(phDDept.split(","));
+        arrays.add(workExperience);
+        arrays.add(bachelorDept);
+        arrays.add(masterDept);
+        arrays.add(phDDept);
         
         // Add items to tableNames
         tableNames.add("universities");
@@ -74,8 +72,8 @@ public class ResumeService {
                 scores.put(array[i], array.length - 1 - i);
             }
         } else if ("workExperience".equals(tableName)) {
-            for (int i = 0; i < array.length - 1; i++) {
-                scores.put(array[i], i + 1);
+            for (int i = 0; i < array.length; i++) {
+                scores.put(array[i], i);
             }
         } else if ("universities".equals(tableName) || 
                    "bachelorDept".equals(tableName) || 
@@ -98,7 +96,7 @@ public class ResumeService {
         }
         
         if ("workExperience".equals(tableName)) {
-            scores.put("more years", 8);
+            scores.put("more years", 9);
         }
         
         return scores;
